094aa4c7d9ad1d12f3c35610fadb4f1bf0b22c4f
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-13 19:42:15 +0530

    Update BMS_checker.cpp
    Corrected the method call and paramters

--- BMS_checker.cpp
+++ BMS_checker.cpp
@@ -44,28 +44,11 @@ bool BMS::BMS::checkRange(float chargeRate)
 bool BMS::BMS::batteryIsOk(float temperature, float soc, float chargeRate)
 {
 
-  tempOk = BMS::BMS::temperatureIsOk;
-  socOk = BMS::BMS::socIsOk;
-  inRange = BMS::BMS::checkRange;
+  tempOk = BMS::BMS::temperatureIsOk(temperature);
+  socOk = BMS::BMS::socIsOk(soc);
+  inRange = BMS::BMS::checkRange(chargeRate);
 
   return tempOk && socOk && inRange;
-
-  // if (temperature < 0 || temperature > 45)
-  // {
-  //   cout << "Temperature out of range!\n";
-  //   return false;
-  // }
-  // else if (soc < 20 || soc > 80)
-  // {
-  //   cout << "State of Charge out of range!\n";
-  //   return false;
-  // }
-  // else if (chargeRate > 0.8)
-  // {
-  //   cout << "Charge Rate out of range!\n";
-  //   return false;
-  // }
-  // return true;
 }
 
 int main()
