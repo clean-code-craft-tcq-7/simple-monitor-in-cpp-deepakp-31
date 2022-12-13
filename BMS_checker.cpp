8d6d6e1f60696b5607b5877a9de0d6397624b003
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-13 19:34:26 +0530

    Update BMS_checker.cpp
    Updated the class methods

--- BMS_checker.cpp
+++ BMS_checker.cpp
@@ -1,29 +1,76 @@
-#include "BMS_checker.hpp"
+#include "checker.hpp"
 
 using namespace std;
 
-bool TelCoColorCoder::batteryIsOk(float temperature, float soc, float chargeRate)
+bool BMS::BMS::temperatureIsOk(float temperature)
 {
   if (temperature < 0 || temperature > 45)
   {
     cout << "Temperature out of range!\n";
     return false;
   }
-  else if (soc < 20 || soc > 80)
+  else
+  {
+    return true;
+  }
+}
+
+bool BMS::BMS::socIsOk(float soc)
+{
+  if (soc < 20 || soc > 80)
   {
     cout << "State of Charge out of range!\n";
     return false;
   }
-  else if (chargeRate > 0.8)
+  else
+  {
+    return true;
+  }
+}
+
+bool BMS::BMS::checkRange(float chargeRate)
+{
+  if (chargeRate > 0.8)
   {
     cout << "Charge Rate out of range!\n";
     return false;
   }
-  return true;
+  else
+  {
+    return true;
+  }
+}
+
+bool BMS::BMS::batteryIsOk(float temperature, float soc, float chargeRate)
+{
+
+  tempOk = BMS::BMS::temperatureIsOk;
+  socOk = BMS::BMS::socIsOk;
+  inRange = BMS::BMS::checkRange;
+
+  return tempOk && socOk && inRange;
+
+  // if (temperature < 0 || temperature > 45)
+  // {
+  //   cout << "Temperature out of range!\n";
+  //   return false;
+  // }
+  // else if (soc < 20 || soc > 80)
+  // {
+  //   cout << "State of Charge out of range!\n";
+  //   return false;
+  // }
+  // else if (chargeRate > 0.8)
+  // {
+  //   cout << "Charge Rate out of range!\n";
+  //   return false;
+  // }
+  // return true;
 }
 
 int main()
 {
-  assert(TelCoColorCoder::batteryIsOk(25, 70, 0.7) == true);
-  assert(TelCoColorCoder::batteryIsOk(50, 85, 0) == false);
+  BMS::BMS bms;
+  assert(bms.batteryIsOk(25, 70, 0.7) == true);
+  assert(bms.batteryIsOk(50, 85, 0) == false);
 }
