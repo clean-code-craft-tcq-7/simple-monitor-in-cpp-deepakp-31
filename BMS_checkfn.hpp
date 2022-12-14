36892ee6dc9294b3311f286549b7726a949e2ce7
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-14 14:16:33 +0530

    Update BMS_checkfn.hpp
    Removed error of return type

--- BMS_checkfn.hpp
+++ BMS_checkfn.hpp
@@ -1,8 +1,10 @@
 #ifndef CHECK_FN
 #define CHECK_FN
+#include <string>
+#include <iostream>
 
-checkFunction(float variable, float min, float max, string outputString);
+bool checkFunction(float variable, float min, float max, std::string outputString);
 
-checkFunction(float variable, float limit, string outputString);
+bool checkFunction(float variable, float limit, std::string outputString);
 
 #endif 
