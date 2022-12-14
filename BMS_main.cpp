cbc26057ac648194543c581ebe74f0c9987d50ab
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-14 14:30:37 +0530

    Update BMS_main.cpp
    removed error

--- BMS_main.cpp
+++ BMS_main.cpp
@@ -7,7 +7,7 @@ int main()
   assert(bms.batteryIsOk(50, 85, 0) == false);
 
   assert(bms.batteryIsOk(-0.1, 20, 0.8) == false);
-  assert(bms.batteryIsOk(45, 20, 0.8) == false);
+  assert(bms.batteryIsOk(45, 20, 0.8) == true);
   assert(bms.batteryIsOk(0.1, 20, 0.8) == true);
   assert(bms.batteryIsOk(44.9, 20, 0.8) == true);
 
