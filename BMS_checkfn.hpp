90582b3dba275466e850220d3d04984a1884b602
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-14 13:35:27 +0530

    Create BMS_checkfn.hpp
    Added header file

---
+++ BMS_checkfn.hpp
@@ -0,0 +1,8 @@
+#ifndef CHECK_FN
+#define CHECK_FN
+
+checkFunction(float variable, float min, float max, string outputString);
+
+checkFunction(float variable, float limit, string outputString);
+
+#endif 
