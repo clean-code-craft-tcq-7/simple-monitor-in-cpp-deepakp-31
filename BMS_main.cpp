841f3fc93e3d49a9d388de42757118694363e7d7
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-14 15:11:18 +0530

    Update BMS_main.cpp
    Updated limits in assert

--- BMS_main.cpp
+++ BMS_main.cpp
@@ -11,7 +11,7 @@ int main()
   assert(bms.batteryIsOk(0.1, 20, 0.8) == true);
   assert(bms.batteryIsOk(44.9, 20, 0.8) == true);
 
-  assert(bms.batteryIsOk(0, 80, 0.8) == false);
+  assert(bms.batteryIsOk(0, 80.1, 0.8) == false);
   assert(bms.batteryIsOk(0, 20.1, 0.8) == true);
   assert(bms.batteryIsOk(0, 79.9, 0.8) == true);
 
