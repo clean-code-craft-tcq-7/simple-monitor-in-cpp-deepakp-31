e0225e05eb7ed333d1685cd2321a0c72211a28b2
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-14 13:01:07 +0530

    Create BMS_parameters.hpp
    Added Parameter file

---
+++ BMS_parameters.hpp
@@ -0,0 +1,16 @@
+#ifndef BMS_PARAM
+#define BMS_PARAM
+
+// Temperature
+float temperatureMin = 0.0;
+float temperatureMax = 45.0;
+
+// SOC
+float socMin = 20.0;
+float socMax = 80.0;
+
+// charge Rate Limit
+float chargeRateLimit = 0.8;
+
+#endif
+
