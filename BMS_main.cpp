0c339b1de5743511135ae2a0a24aaf6d6f52c1ac
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-14 14:25:31 +0530

    Update BMS_main.cpp
    assert

--- BMS_main.cpp
+++ BMS_main.cpp
@@ -5,5 +5,16 @@ int main()
   BMS::BMS bms;
   assert(bms.batteryIsOk(25, 70, 0.7) == true);
   assert(bms.batteryIsOk(50, 85, 0) == false);
+
+  assert(bms.batteryIsOk(0, 20, 0.8) == false);
+  assert(bms.batteryIsOk(45, 20, 0.8) == false);
+  assert(bms.batteryIsOk(0.1, 20, 0.8) == true);
+  assert(bms.batteryIsOk(44.9, 20, 0.8) == true);
+
+  assert(bms.batteryIsOk(0, 80, 0.8) == false);
+  assert(bms.batteryIsOk(0, 20.1, 0.8) == true);
+  assert(bms.batteryIsOk(0, 79.9, 0.8) == true);
+
+  assert(bms.batteryIsOk(0, 20, 0.79) == true);
 }
 
