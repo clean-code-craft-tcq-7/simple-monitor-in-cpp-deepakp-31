8142d804d10abf09d13399e1284f4ec2533537f5
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-19 19:29:11 +0530

    Added function to convert input to different units

--- BMS_checker.cpp
+++ BMS_checker.cpp
@@ -38,48 +38,64 @@
 //   return isBatteryParameterinRange(chargeRate, chargeRateLimit, outputString);
 // }
 
+float BMS::BMS::convertBateryParam(float paramatervalue, float scaleFactor, float shiftFactor)
+{
+  return ((scaleFactor * paramatervalue) + shiftFactor);
+}
+
+// WITHOUT WARNING TOLERANCE
 bool BMS::BMS::temperatureIsOk(float temperature)
 {
   std::string outputString = "TEMPERATURE";
-  return isBatteryParameterWithinLimits(temperature, temperatureMin, temperatureMax, outputString);
+  return isBatteryParameterWithinLimits(convertBateryParam(temperature, temperatureScaleFactor, temperatureShiftFactor),
+                                        temperatureMin, temperatureMax, outputString);
 }
 
 bool BMS::BMS::socIsOk(float soc)
 {
   std::string outputString = "SOC";
-  return isBatteryParameterWithinLimits(soc, socMin, socMax, outputString);
+  return isBatteryParameterWithinLimits(convertBateryParam(soc, socScaleFactor, socShiftFactor),
+                                        socMin, socMax, outputString);
 }
 
 bool BMS::BMS::checkChargeRate(float chargeRate)
 {
   std::string outputString = "CHARGERATE";
-  return isBatteryParameterinRange(chargeRate, chargeRateLimit, outputString);
+  return isBatteryParameterinRange(convertBateryParam(chargeRate, chargeRateScaleFactor, chargeRateShiftFactor),
+                                   chargeRateLimit, outputString);
 }
 
+// WITH WARNING TOLERANCE
 bool BMS::BMS::temperatureIsOk(float temperature, float warningPercent)
 {
   std::string outputString = "TEMPERATURE";
-  return isBatteryParameterWithinLimits(temperature, temperatureMin, temperatureMax, outputString);
+  return isBatteryParameterWithinLimitsWithWarning(
+      convertBateryParam(temperature, temperatureScaleFactor, temperatureShiftFactor),
+      temperatureTolerance, temperatureMin, temperatureMax, outputString);
 }
 
-bool BMS::BMS::socIsOk(float soc, float warningPercent)
+bool BMS::BMS::socIsOk(float soc)
 {
   std::string outputString = "SOC";
-  return isBatteryParameterWithinLimits(soc, socMin, socMax, outputString);
+  return isBatteryParameterWithinLimitsWithWarning(
+      convertBateryParam(soc, socScaleFactor, socShiftFactor),
+      socTolerance, socMin, socMax, outputString);
 }
 
-bool BMS::BMS::checkChargeRate(float chargeRate, float warningPercent)
+bool BMS::BMS::checkChargeRate(float chargeRate)
 {
   std::string outputString = "CHARGERATE";
-  return isBatteryParameterinRange(chargeRate, chargeRateLimit, outputString);
+  return isBatteryParameterinRangeWithWarning(
+      convertBateryParam(chargeRate, chargeRateScaleFactor, chargeRateShiftFactor),
+      chargeRateTolerance, chargeRateLimit, outputString);
 }
 
 bool BMS::BMS::batteryIsOk(float temperature, float soc, float chargeRate)
 {
 
-  tempOk = BMS::BMS::temperatureIsOk(temperature);
-  socOk = BMS::BMS::socIsOk(soc);
-  inRange = BMS::BMS::checkChargeRate(chargeRate);
+  tempOk = BMS::BMS::temperatureIsOk(temperature, temperatureTolerance);
+  socOk = BMS::BMS::socIsOk(soc, socTolerance);
+  inRange = BMS::BMS::checkChargeRate(chargeRate, chargeRateTolerance);
 
   return tempOk && socOk && inRange;
 }
