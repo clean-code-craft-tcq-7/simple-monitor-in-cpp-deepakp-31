3619906702e82fabc546dbf36e9ec9cf8a368534
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-19 20:13:07 +0530

    Update BMS_parameters.hpp
    Updating paramters

--- BMS_parameters.hpp
+++ BMS_parameters.hpp
@@ -4,13 +4,21 @@
 // Temperature
 float temperatureMin = 0.0;
 float temperatureMax = 45.0;
+float temperatureTolerance = 0.05;
+float temperatureScaleFactor = 5 / 9;
+float temperatureShiftFactor = (-32 * 5) / 9;
 
 // SOC
 float socMin = 20.0;
 float socMax = 80.0;
+float socTolerance = 0.05;
+float socScaleFactor = 1.0;
+float socShiftFactor = 0.0;
 
 // charge Rate Limit
 float chargeRateLimit = 0.8;
+float chargeRateTolerance = 0.05;
+float chargeRateScaleFactor = 1.0;
+float chargeRateShiftFactor = 0.0;
 
 #endif
-
