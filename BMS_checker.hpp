ad79d865a88d4d2a08e422fac1d9ab79a6506425
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-19 19:29:32 +0530

    Update BMS_checker.hpp

--- BMS_checker.hpp
+++ BMS_checker.hpp
@@ -10,11 +10,20 @@ namespace BMS
     class BMS
     {
     public:
+        //Assuming the parameter is just transformation achieved by simple multiplication and shifting
+        float convertBateryParam(float paramatervalue, float scaleFactor, float shiftFactor);
+
         bool temperatureIsOk(float temperature);
 
         bool socIsOk(float soc);
 
         bool checkChargeRate(float chargeRate);
+
+        bool temperatureIsOk(float temperature, float warningPercent);
+
+        bool socIsOk(float soc, float warningPercent);
+
+        bool checkChargeRate(float chargeRate, float warningPercent);
 
         bool batteryIsOk(float temperature, float soc, float chargeRate);
 
