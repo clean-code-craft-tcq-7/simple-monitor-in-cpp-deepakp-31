6a527179982ba57651d0d3a048fdaedf1a8f947f
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2022-12-19 20:44:29 +0530

    Updating assert statement wrt Fahrenheit

--- BMS_main.cpp
+++ BMS_main.cpp
@@ -3,26 +3,42 @@
 int main()
 {
   BMS::BMS bms;
-  assert(bms.batteryIsOk(25, 70, 0.7) == true);
-  assert(bms.batteryIsOk(50, 85, 0) == false);
+  bool Faranheit = true;
 
-  assert(bms.batteryIsOk(-0.1, 20, 0.8) == false);
-  assert(bms.batteryIsOk(45, 20, 0.8) == true);
-  assert(bms.batteryIsOk(0.1, 20, 0.8) == true);
-  assert(bms.batteryIsOk(44.9, 20, 0.8) == true);
+  if (Faranheit)
+  {
+    assert(bms.batteryIsOk(77, 70, 0.7) == true);
+    assert(bms.batteryIsOk(122, 85, 0) == false);
 
-  assert(bms.batteryIsOk(0, 80.1, 0.8) == false);
-  assert(bms.batteryIsOk(0, 20.1, 0.8) == true);
-  assert(bms.batteryIsOk(0, 79.9, 0.8) == true);
+    // Temperature
+    assert(bms.temperatureIsOk(31.82) == false);
+    // assert(bms.temperatureIsOk(0) == true);
+    // assert(bms.temperatureIsOk(45) == true);
+    // assert(bms.temperatureIsOk(45.1) == false);
+  }
 
-  assert(bms.batteryIsOk(0, 20, 0.79) == true);
+  else
+  {
+    assert(bms.batteryIsOk(25, 70, 0.7) == true);
+    assert(bms.batteryIsOk(50, 85, 0) == false);
 
-  // Temperature
-  assert(bms.temperatureIsOk(-0.1) == false);
-  assert(bms.temperatureIsOk(0) == true);
-  assert(bms.temperatureIsOk(45) == true);
-  assert(bms.temperatureIsOk(45.1) == false);
+    assert(bms.batteryIsOk(-0.1, 20, 0.8) == false);
+    assert(bms.batteryIsOk(45, 20, 0.8) == true);
+    assert(bms.batteryIsOk(0.1, 20, 0.8) == true);
+    assert(bms.batteryIsOk(44.9, 20, 0.8) == true);
 
+    assert(bms.batteryIsOk(0, 80.1, 0.8) == false);
+    assert(bms.batteryIsOk(0, 20.1, 0.8) == true);
+    assert(bms.batteryIsOk(0, 79.9, 0.8) == true);
+
+    assert(bms.batteryIsOk(0, 20, 0.79) == true);
+
+    // Temperature
+    assert(bms.temperatureIsOk(-0.1) == false);
+    assert(bms.temperatureIsOk(0) == true);
+    assert(bms.temperatureIsOk(45) == true);
+    assert(bms.temperatureIsOk(45.1) == false);
+  }
   // State of Charge
   assert(bms.socIsOk(19.9) == false);
   assert(bms.socIsOk(20) == true);
@@ -33,4 +49,3 @@ int main()
   assert(bms.checkChargeRate(0.81) == false);
   assert(bms.checkChargeRate(0.8) == true);
 }
-
