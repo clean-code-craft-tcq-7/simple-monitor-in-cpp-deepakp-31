21bceff050c51dd6d794be6171b664b2c44f0e58
deepakp-31 <115739672+deepakp-31@users.noreply.github.com>
2023-01-04 16:59:29 +0530

    Update BMS_checker.cpp
    Removed commented code

--- BMS_checker.cpp
+++ BMS_checker.cpp
@@ -2,42 +2,6 @@
 #include "BMS_parameters.hpp"
 #include "BMS_checkfn.hpp"
 
-// bool BMS::BMS::temperatureIsOk(float temperature)
-// {
-//   std::string outputString = "Temperature out of range!";
-//   return isBatteryParameterWithinLimits(temperature, temperatureMin, temperatureMax, outputString);
-// }
-
-// bool BMS::BMS::socIsOk(float soc)
-// {
-//   std::string outputString = "State of Charge out of range!";
-//   return isBatteryParameterWithinLimits(soc, socMin, socMax, outputString);
-// }
-
-// bool BMS::BMS::checkChargeRate(float chargeRate)
-// {
-//   std::string outputString = "Charge Rate out of range!";
-//   return isBatteryParameterinRange(chargeRate, chargeRateLimit, outputString);
-// }
-
-// bool BMS::BMS::temperatureIsOk(float temperature, float warningPercent)
-// {
-//   std::string outputString = "Temperature out of range!";
-//   return isBatteryParameterWithinLimits(temperature, temperatureMin, temperatureMax, outputString);
-// }
-
-// bool BMS::BMS::socIsOk(float soc, float warningPercent)
-// {
-//   std::string outputString = "State of Charge out of range!";
-//   return isBatteryParameterWithinLimits(soc, socMin, socMax, outputString);
-// }
-
-// bool BMS::BMS::checkChargeRate(float chargeRate, float warningPercent)
-// {
-//   std::string outputString = "Charge Rate out of range!";
-//   return isBatteryParameterinRange(chargeRate, chargeRateLimit, outputString);
-// }
-
 float BMS::BMS::convertBatteryParam(float paramatervalue, float scaleFactor, float shiftFactor)
 {
   return ((scaleFactor * paramatervalue) + shiftFactor);
